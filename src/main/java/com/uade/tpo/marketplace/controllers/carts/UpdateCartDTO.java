3e12b2ebaa1262780004d31da0a8f06b11e3fc38
Camila Nani <caminani24@gmail.com>
2025-06-11 23:03:35 -0300

    arreglo carrito

---
+++ src/main/java/com/uade/tpo/marketplace/controllers/carts/UpdateCartDTO.java
@@ -0,0 +1,22 @@
+package com.uade.tpo.marketplace.controllers.carts;
+
+public class UpdateCartDTO {
+    private Long bookId;
+    private Integer quantity;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+} 
